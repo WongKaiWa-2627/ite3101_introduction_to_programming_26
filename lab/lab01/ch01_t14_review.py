a166e2cf0f257ea21b9cd4eaea0e438b59cac180
WongKaiWa-2627 <k.w.wong2005223@gmail.com>
2026-09-24 08:51:33 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -3,4 +3,7 @@ exercises_completed = 13
 
 points_per_exercise = 5
 
-point_total
+point_total = 100
+point_total += exercises_completed * points_per_exercise
+
+print("I got " + str(point_total) + " points!")
